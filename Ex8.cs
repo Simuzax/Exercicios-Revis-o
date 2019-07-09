0ab5dfc525f8107c4c4269707bc2baa37746d0f8
Simuzax <simuzax@gmail.com>
2019-07-09 11:56:01 -0300

    190709

--- Ex8.cs
+++ Ex8.cs
@@ -36,6 +36,11 @@ namespace ConsoleApp4
             }
         }
 
+        public void MostrarDados()
+        {
+            Console.WriteLine("HP e Força atual do " + nome + ": " + Hp + " e " + Força);
+        }
+
         public virtual void Atacar(Personagem alvo)
         {
             alvo.Hp -= Força;
@@ -58,12 +63,14 @@ namespace ConsoleApp4
             alvo.Hp -= (int)(alvo.Hp * 0.3);
         }
 
-        void RoubarVida(Personagem alvo)
+        public int RoubarVida(Personagem alvo)
         {
             int lifesteal = (int)(alvo.Hp * 0.1);
 
-            alvo.Hp -= (int)(alvo.Hp * 0.1);
+            alvo.Hp -= lifesteal;
             Hp += lifesteal;
+
+            return lifesteal;
         }
     }
 
@@ -71,6 +78,8 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            int guerreiroHpBase, guerreiroForçaBase, inimigoHpBase, inimigoForçaBase, lifesteal;
+
             Guerreiro guerreiro = new Guerreiro();
             Inimigo inimigo = new Inimigo();
 
@@ -81,18 +90,96 @@ namespace ConsoleApp4
             inimigo.nome = Console.ReadLine();
 
             Console.WriteLine("-------------------------");
-            Console.WriteLine("Digite o HP do Guerreiro:");
-            guerreiro.Hp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o HP de " + guerreiro.nome + ":");
+            guerreiroHpBase = Convert.ToInt32(Console.ReadLine());
+            guerreiro.Hp = guerreiroHpBase;
 
-            Console.WriteLine("Digite a Força do Guerreiro:");
-            guerreiro.Força = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a Força de " + guerreiro.nome + ":");
+            guerreiroForçaBase = Convert.ToInt32(Console.ReadLine());
+            guerreiro.Força = guerreiroForçaBase;
 
             Console.WriteLine("-----------------------");
-            Console.WriteLine("Digite o HP do Inimigo:");
-            inimigo.Hp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o HP de " + inimigo.nome + ":");
+            inimigoHpBase = Convert.ToInt32(Console.ReadLine());
+            inimigo.Hp = inimigoHpBase;
 
-            Console.WriteLine("Digite a Força do Inimigo:");
-            inimigo.Força = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a Força de " + inimigo.nome + ":");
+            inimigoForçaBase = Convert.ToInt32(Console.ReadLine());
+            inimigo.Força = inimigoForçaBase;
+
+            Console.WriteLine();
+
+            int comando = -1;
+
+            while (comando != 0)
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("O que deseja fazer agora?");
+                Console.WriteLine("(Digite um dos valores a seguir para realizar uma ação)");
+                Console.WriteLine();
+                Console.WriteLine("1 - " + guerreiro.nome + " atacar " + inimigo.nome);
+                Console.WriteLine("2 - " + inimigo.nome + " atacar " + guerreiro.nome);
+                Console.WriteLine("3 - " + guerreiro.nome + " aumenta sua força");
+                Console.WriteLine("4 - " + inimigo.nome + " rouba vida do " + guerreiro.nome);
+                Console.WriteLine("5 - Mostrar dados do " + guerreiro.nome + " e do " + inimigo.nome);
+                Console.WriteLine("6 - Resetar os valores do " + guerreiro.nome + " para os valores base");
+                Console.WriteLine("7 - Resetar os valores do " + inimigo.nome + " para os valores base");
+                Console.WriteLine("0 - Sair do Programa");
+                Console.WriteLine();
+
+                comando = Convert.ToInt32(Console.ReadLine());
+
+                switch (comando)
+                {
+                    case 1:
+                        guerreiro.MostrarDados();
+                        inimigo.MostrarDados();
+                        Console.WriteLine(guerreiro.nome + " ataca " + inimigo.nome);
+                        guerreiro.Atacar(inimigo);
+                        Console.WriteLine("O HP de " + inimigo.nome + " diminui para: " + inimigo.Hp);
+                        break;
+                    case 2:
+                        guerreiro.MostrarDados();
+                        inimigo.MostrarDados();
+                        Console.WriteLine(inimigo.nome + " ataca " + guerreiro.nome);
+                        inimigo.Atacar(guerreiro);
+                        Console.WriteLine("O HP de " + guerreiro.nome + " diminui para: " + guerreiro.Hp);
+                        break;
+                    case 3:
+                        Console.WriteLine("HP e Força atual: " + guerreiro.Hp + ", " + guerreiro.Força);
+                        Console.WriteLine(guerreiro.nome + " aumenta sua força");
+                        guerreiro.AumentarForça();
+                        Console.WriteLine("Seu HP diminui para: " + guerreiro.Hp);
+                        Console.WriteLine("Sua Força aumenta para: " + guerreiro.Força);
+                        break;
+                    case 4:
+                        guerreiro.MostrarDados();
+                        inimigo.MostrarDados();
+                        Console.WriteLine(inimigo.nome + " rouba vida");
+                        lifesteal = inimigo.RoubarVida(guerreiro);
+                        Console.WriteLine(inimigo.nome + " se cura em: " + lifesteal);
+                        Console.WriteLine(guerreiro.nome + " recebe " + lifesteal + " de dano");
+                        break;
+                    case 5:
+                        guerreiro.MostrarDados();
+                        Console.WriteLine();
+                        inimigo.MostrarDados();
+                        break;
+                    case 6:
+                        guerreiro.Hp = guerreiroHpBase;
+                        guerreiro.Força = guerreiroForçaBase;
+                        Console.WriteLine("Valores resetados");
+                        break;
+                    case 7:
+                        inimigo.Hp = inimigoHpBase;
+                        inimigo.Força = inimigoForçaBase;
+                        Console.WriteLine("Valores resetados");
+                        break;
+                    case 0:
+                        comando = 0;
+                        break;
+                }
+            }
         }
     }
 }
